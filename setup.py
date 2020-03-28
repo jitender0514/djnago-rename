146e1bf801a94d996a73d7bae2869f38c4159f61
Jitender <jitender@dp360crm.com>
2020-03-28 14:40:27 +0530

    updated the setting.py file

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+import rename_project
+
+
+setup(
+    name='rename_project',
+    packages = ['rename_project'], 
+    license='BSD',
+    version=rename_project.__version__,
+    description='RenameProject is a Django app to renaming the project of boilerplate',
+    author='Jitender Singh',
+    author_email='jitender0514@gmail.com',
+    include_package_data=True,
+    download_url='https://github.com/jitender0514/djnago-rename/archive/0.1.tar.gz',
+    url='https://github.com/jitender0514/djnago-rename',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
